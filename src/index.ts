17e8a477aec35c49029f86e2ecc6211fe2cdcc5c
SyriusGit <henridoss.miranda@gmail.com>
2023-07-11 21:29:11 -0300

    feat: server express ruinning

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Temporr do chef");
+import express from "express";
+import {config} from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 3333;
+app.get("/teste", (request, response) => {response.send("Atenção terráqueos, o Grêmio vai sair campeão!!!")})
+app.listen(port, () => console.log(`listening on port ${port}!`));
